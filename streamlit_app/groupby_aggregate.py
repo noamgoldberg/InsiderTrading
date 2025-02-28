21e32e4332d706ceddaf29917e0dacc3ba2b6ac3
noamgoldberg <noamgoldberg2@gmail.com>
2025-02-28 02:59:33 -0500

    [year] Added back 'Year' groupby

--- streamlit_app/groupby_aggregate.py
+++ streamlit_app/groupby_aggregate.py
@@ -8,7 +8,7 @@ GROUPBY_OPTIONS_DICT = {
     "Company": ("Company Name", lambda df: df.groupby("Company Name")),
     "Day": ("Trade Date", lambda df: df.groupby("Trade Date")),
     "Month": ("Trade Date", lambda df: df.groupby(df["Trade Date"].dt.to_period("M").astype(str).astype("category"))),
-    # "Year": ("Trade Date", lambda df: df.groupby(df["Trade Date"].dt.year.astype(int))),
+    "Year": ("Trade Date", lambda df: df.groupby(df["Trade Date"].dt.year.astype(int))),
 }
 
 AGGREGATIONS = {
